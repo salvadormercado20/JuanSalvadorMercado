fee862933025da55a3c54f8f129c47c84daa1691
Juan SalvadorMercado <juansalvadormercado20@gmail.com>
2019-11-16 13:11:50 -0500

    using foor loops

--- getting-started/start-here/src/app/app.component.ts
+++ getting-started/start-here/src/app/app.component.ts
@@ -6,11 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  event: any =
-    {
-      time: "08:00",
-      subject: "Breakfast with Simon",
-      location: "Lounge Caffe",
-      description: "Discuss Q3 targets"
-    };
+  events: Array<any> = [
+    {time: '08:00', subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
+    {time: '08:30', subject: 'Daily Standup Meeting (recurring)', location: 'Warsaw Spire Office'},
+    {time: '09:00', subject: 'Call with HRs'},
+    {time: '12:00', subject: 'Lunch with Timmoty', location: 'Canteen', description: 'Project evalutation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a'},
+  ];
 }
